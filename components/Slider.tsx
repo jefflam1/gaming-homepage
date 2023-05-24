34752bfa1f0f5f38139185f252da787f36209aea
Jeff Lam <lmh.jeff@gmail.com>
2023-05-25 02:07:00 +0800

    modified slider component

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -3,39 +3,90 @@
 import { Carousel, Rate } from "antd";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import Slide1 from "../public/slide1.png";
 import Slide2 from "../public/slide2.png";
 import Slide3 from "../public/slide3.png";
 
 const Slider = () => {
+  const useWindowSize = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      if (typeof window !== "undefined") {
+        const handleResize = () => {
+          setWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, []);
+
+    return width;
+  };
+
+  const innerWidth = useWindowSize();
+  console.log(innerWidth);
   return (
-    <div className="w-full mt-12">
-      <Carousel arrows={true} dots={{ className: "abc" }}>
-        <div className="w-full  flex justify-center items-center">
-          <Image
-            src={Slide1}
-            alt={""}
-            object-fit="contain"
-            className="m-auto"
-          />
+    <div className="w-full mt-12 md:mt-20 justify-center items-center">
+      {innerWidth < 460 ? (
+        <Carousel arrows={false} dots={{ className: "abc" }}>
+          <div className="w-full  flex justify-center items-center">
+            <Image
+              src={Slide1}
+              alt={""}
+              object-fit="contain"
+              className="m-auto scale-90"
+            />
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <Image
+              src={Slide2}
+              alt={""}
+              object-fit="contain"
+              className="m-auto scale-90"
+            />
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <Image
+              src={Slide3}
+              alt={""}
+              object-fit="contain"
+              className="m-auto scale-90 mb-16"
+            />
+          </div>
+        </Carousel>
+      ) : (
+        <div className="flex flex-row justify-center space-x-4 items-center">
+          <div>
+            <Image
+              src={Slide1}
+              alt={""}
+              object-fit="contain"
+              className="m-auto"
+            />
+          </div>
+          <div>
+            <Image
+              src={Slide2}
+              alt={""}
+              object-fit="contain"
+              className="m-auto"
+            />
+          </div>
+          <div>
+            <Image
+              src={Slide3}
+              alt={""}
+              object-fit="contain"
+              className="m-auto"
+            />
+          </div>
         </div>
-        <div className="w-full flex justify-center items-center">
-          <Image
-            src={Slide2}
-            alt={""}
-            object-fit="contain"
-            className="m-auto"
-          />
-        </div>
-        <div className="w-full flex justify-center items-center">
-          <Image
-            src={Slide3}
-            alt={""}
-            object-fit="contain"
-            className="m-auto mb-16"
-          />
-        </div>
-      </Carousel>
+      )}
     </div>
   );
 };
