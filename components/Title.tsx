f80e1a2acd19189377d179e888934a9f68aae2a9
Jeff Lam <lmh.jeff@gmail.com>
2023-05-24 13:31:11 +0800

    fixed position

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,17 +3,16 @@ import TitleIcon from "../public/titleIcon.png";
 
 const Title = () => {
   return (
-    <div className="flex flex-row relative mt-8">
+    <div className="flex flex-row justify-center relative mt-16">
       <Image
         src={TitleIcon}
         alt={"main logo"}
         object-fit="contain"
-        className="absolute -top-8 -left-12 md:-top-9 md:-left-14 w-12 h-14 md:w-14 md:h-16"
+        className="absolute -top-16 -left-12 md:-top-12 md:left-32 scale-[0.6] md:scale-100"
       />
 
-      <div className="font-bebasNeue text-4xl md:text-7xl text-center">
-        Next Generation Get Your
-        <br /> Desierd Rank Now
+      <div className="break-words w-72 md:w-3/5 font-bebasNeue text-4xl md:text-7xl text-center">
+        Next Generation Get Your Desierd Rank Now
       </div>
     </div>
   );
